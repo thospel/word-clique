14038e6321c6dee143e02f46b22f76717b48de1c
Ton Hospel <t.hospel@deutsche-glasfaser.de>
2022-08-05 18:49:51 +0200

    Add setup.cfg, setup.dist. Add conda.yml. More code ceanups

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Purely for backward compatibility. All our information is in setup.cfg
+# and modern build systems do not need this script anymore
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
